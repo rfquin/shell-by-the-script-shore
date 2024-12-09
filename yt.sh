90af7b3fa34455c9f983609d35fcb18f01685bbf
quinny <rfquinny@gmail.com>
2024-12-10 02:26:51 +1100

    [ yt.sh ] remove comments

--- yt.sh
+++ yt.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 while getopts "hvf:" flag; do
  case $flag in
-   playlist-mp3) # Handle the -h flag
-
+   playlist-mp3)
 	if [ "$#" -ne 1 ]; then
 		echo "Usage: $0 --audio <playlist_url>"
    		exit 1
 	fi
 	yt-dlp -f bestaudio --extract-audio --audio-format mp3 '$1'
-   # Display script help information
    ;;
    \?)
    echo "Incorrect flag passed" 
