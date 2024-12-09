ab5e9e35aea6e2e77bdd111ec90a1ee1cc158087
Ben Quin <rfquinny@gmail.com>
2024-12-10 02:11:19 +1100

    [yt.sh] Add audio playlist download

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # shell-by-the-script-shore
+<<<<<<< HEAD
 
 A collection of useful shell scripts so I don't lose them
+=======
+>>>>>>> dc34a01 ([yt.sh] Add audio playlist download)
---
+++ yt.sh
@@ -0,0 +1,16 @@
+while getopts "hvf:" flag; do
+ case $flag in
+   playlist-mp3) # Handle the -h flag
+
+	if [ "$#" -ne 1 ]; then
+		echo "Usage: $0 --audio <playlist_url>"
+   		exit 1
+	fi
+	yt-dlp -f bestaudio --extract-audio --audio-format mp3 '$1'
+   # Display script help information
+   ;;
+   \?)
+   echo "Incorrect flag passed" 
+   ;;
+ esac
+done
